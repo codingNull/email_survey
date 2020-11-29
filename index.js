3faf33dfbeac2a4da5d6e717aa52c798179ad140
lyl <lylcnau@gmail.com>
2020-11-30 01:05:30 +1100

    fix port problem

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.Port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log(`server listen at port ${PORT}`);
